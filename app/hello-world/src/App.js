2a1e0d3b8ec683a10e3cf7579093ae35cfdad430
andreu casals <acasalsr@gmail.com>
2022-02-03 19:19:27 +0100

    first component test

--- app/hello-world/src/App.js
+++ app/hello-world/src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Hello world!
+          <br />
+          <p>this is my first line of code.</p>
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <p className="testClass">Hola que tal? </p>
       </header>
     </div>
   );
