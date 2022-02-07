01ea259ae03565db797db0eed1a2e751a5319dd5
andreu casals <acasalsr@gmail.com>
2022-02-07 19:56:57 +0100

    props-exercice

--- authors-app/authors-app/src/App.js
+++ authors-app/authors-app/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+function Salutacio({ nom }) {
+  return <h1>hola {nom}</h1>;
+}
+
+/*
+Funció temps -> estacion -> és per definir la variable + [key] que qualsevol
+objecte rep.  
+*/
+
+function Temps({ estacion }) {
+  return (
+    <div>
+      <h2>Ciutat: {estacion.ciutat}</h2>
+      <p>Avui fa {estacion.fa}.</p>
+      <p>La temperatura és de {estacion.temperatura}º</p>
+    </div>
+  );
+} 
+export default function App() {
+  let name = "Nenet";
+  let barcelona = {
+    ciutat: "barcelona",
+    fa: "Nuvol",
+    temperatura: 10,
+  };
+  let londres = {
+    ciutat: "Londres",
+    fa: "pluja",
+    temperatura: 4,
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Salutacio nom={name} />
+        {/* Estacio és la funcio i Barcelona és el que volem ensenyar */}
+        <Temps estacion={barcelona} />
+        <Temps estacion={londres} />
       </header>
     </div>
   );
 }
-
-export default App;
---
+++ authors-app/authors-app/src/cars.js
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+  {
+    brand: "Citroen",
+    model: "c4",
+    engine: 1.4,
+    imageURL:
+      "https://www.citroen.es/content/dam/citroen/master/b2c/models/new-c4-e/visualizer/front-view/New%20E-C4%20and%20C4_0MP00NWP_Blanc%20Banquise_FR_1280_720.png",
+  },
+  {
+    brand: "Renault",
+    model: "Scenic",
+    engine: 1.6,
+    imageURL:
+      "https://www.autobild.es/sites/autobild.es/public/styles/main_element/public/dc/fotos/Renault-Scenic_2017_C01.jpg?itok=DvyqBgCa",
+  },
+  {
+    brand: "Seat",
+    model: "Leon",
+    engine: 1.4,
+    imageURL: "",
+  },
+  {
+    brand: "Peugeot",
+    model: "2008",
+    engine: 1.1,
+    imageURL: "",
+  },
+  {
+    brand: "Audi",
+    model: "A4",
+    engine: 1.9,
+    imageURL: "",
+  },
+  {
+    brand: "BMW",
+    model: "330i",
+    engine: 3.0,
+    imageURL: "",
+  },
+];
