eb8d2f43571d5c1599b599fdff8f62fdb7de9cd0
andreu casals <acasalsr@gmail.com>
2022-02-14 19:06:23 +0100

    calculator done - without style

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -7,26 +7,33 @@ export default function App() {
   const [antic, setAntic] = useState(0);
   const [operacio, setOperacio] = useState("");
   const [display, setDisplay] = useState(0);
-  const entraOperacio = () => {
+  const [novaEntrada, setNovaEntrada] = useState(false);
+  const entraOperacio = (operacio) => {
+    setNovaEntrada(true);
     setAntic(display);
-    setOperacio("+");
+    if (!novaEntrada) calcular();
+    setOperacio(operacio);
   };
   const calcular = () => {
-    if (operacio === "+") {
-      setDisplay(antic + display);
-    }
+    if (operacio === "+") setDisplay(antic + display);
+    if (operacio === "-") setDisplay(antic - display);
+  };
+  const entrarNumero = (numero) => {
+    if (novaEntrada === true) setDisplay(numero);
+    else setDisplay(display * 10 + numero);
+    setNovaEntrada(false);
   };
   return (
     <>
-      <div>
-        ({antic}), {operacio} {display}
-      </div>
+      <div>{display}</div>
       <br />
       {numeros.map((numero) => (
-        <button onClick={() => setDisplay(numero)}>{numero}</button>
+        <button onClick={() => entrarNumero(numero)}>{numero}</button>
       ))}
       <br />
-      <button onClick={entraOperacio}>+</button>
+      <button onClick={() => entraOperacio("+")}>+</button>
+      <button onClick={() => entraOperacio("-")}>-</button>
+      <br />
       <button onClick={calcular}>=</button>
     </>
   );
