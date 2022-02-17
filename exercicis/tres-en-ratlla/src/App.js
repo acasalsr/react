60acd756d16a91568076748c5f20af13240b4795
andreu casals <acasalsr@gmail.com>
2022-02-17 19:04:00 +0100

    spread operator working

--- exercicis/tres-en-ratlla/src/App.js
+++ exercicis/tres-en-ratlla/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import { click } from "@testing-library/user-event/dist/click";
 
 export default function App() {
   /* 
@@ -8,17 +9,42 @@ Route Map.
 2. Crear un index per saber on el usuari ha clicat. 
 3. onClick -> guardar el valor del lloc clicat i assigar aquest al "setElement" 
 
+Convinacions: 
+1. El numero que seleccionem per poder fer el tres en ratlla per comprovar que sigui cert l'ordre ha
+de comprovar que el numero de l'array pulsat primer suma + 3. En cas que aquest sigui a l'array 0,2,4,6,8 -> podem sumar 3 o 4, però el segon pas determinarà quin ordre
+ha de seguir l'array. Si apretem game[0] -> següentment game [4] -> el últim pas ha de ser  game[8] <- NO pot ser game[7]
+
 Problemes.
 1. Com puc fer que un array amb valor a dins no es pugui substitur? [...game]? 
 */
-  //onClick => comprovar si "primer click "
-
   const game = ["", "", "", "", "", "", "", "", ""];
+
+  const [pulsar, setPulsar] = useState(0);
+  const [arr, setArr] = useState(game);
+
+  let clickContainer = (indexArrayMap) => {
+    if (pulsar % 2 === 0) {
+      setPulsar((n) => n + 1);
+      console.log(arr[indexArrayMap]);
+      arr[indexArrayMap] = "X";
+      setArr([...arr]);
+    } else {
+      setPulsar((n) => n + 1);
+      arr[indexArrayMap] = "O";
+      setArr([...arr]);
+    }
+  };
+
   return (
     <>
       <div className="wrapper-tres-en-ratlla">
-        {game.map((grid) => (
-          <div className="container_tres-en-ratlla"></div>
+        {game.map((containers, index) => (
+          <div
+            onClick={() => clickContainer(index)}
+            className="container_tres-en-ratlla"
+          >
+            {arr[index]}
+          </div>
         ))}
       </div>
     </>
