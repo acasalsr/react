3a3e52110a20c93a6954372a5655d5a8ccaca4b1
andreu casals <acasalsr@gmail.com>
2022-02-21 17:18:40 +0100

    layout website

---
+++ router-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <header>Exercici React, Andreu Casals </header>
+      <div class="HolyGrail-body">
+        <main class="HolyGrail-content">content</main>
+        <nav class="HolyGrail-nav">Nav</nav>
+        <aside class="HolyGrail-ads">ads</aside>
+      </div>
+      <footer>…</footer>
+    </>
+  );
+}
