b688bff6233848911f7ae8031ede956b81d016f4
andreu casals <acasalsr@gmail.com>
2022-02-21 19:10:38 +0100

    main-menu

--- router-project/src/App.jsx
+++ router-project/src/App.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Comptador from "./pages/Comptador";
+import Acordeo from "./pages/Acordeo";
+import Carrusel from "./pages/Carrusel";
+import GraellaFotos from "./pages/GraellaFotos";
 
 export default function App() {
   return (
-    <>
-      <header>Exercici React, Andreu Casals </header>
-      <div class="HolyGrail-body">
-        <main class="HolyGrail-content">content</main>
-        <nav class="HolyGrail-nav">Nav</nav>
-        <aside class="HolyGrail-ads">ads</aside>
-      </div>
-      <footer>…</footer>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="comptador" element={<Comptador />} />
+          <Route path="acordeo" element={<Acordeo />} />
+          <Route path="carrusel" element={<Carrusel />} />
+          <Route path="graellaFotos" element={<GraellaFotos />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ router-project/src/pages/Layout.jsx
@@ -0,0 +1,36 @@
+import { Outlet, Link } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <>
+      <header>Exercici React, Andreu Casals </header>
+      <div class="HolyGrail-body">
+        <main class="HolyGrail-content">
+          <Outlet />
+        </main>
+        <nav class="HolyGrail-nav">
+          Nav
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/comptador">Comptador</Link>
+            </li>
+            <li>
+              <Link to="/acordeo">Acordeo</Link>
+            </li>
+            <li>
+              <Link to="/carrusel">Carrusel</Link>
+            </li>
+            <li>
+              <Link to="/graellaFotos">Graella fotos</Link>
+            </li>
+          </ul>
+        </nav>
+        <aside class="HolyGrail-ads">ads</aside>
+      </div>
+      <footer>This is the footer</footer>
+    </>
+  );
+}
