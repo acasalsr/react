6dff2f7d3e797e84ef010b19fc36e9b0d3d7d047
andreu casals <acasalsr@gmail.com>
2022-02-22 19:11:44 +0100

    rellotge digital done

--- exercicis/rellotge-digital/src/App.js
+++ exercicis/rellotge-digital/src/App.js
@@ -1,17 +1,18 @@
 import { useState, useEffect } from "react";
-
+import "./App.css";
 /*
 
 Mostrar l'hora actual (amb segons) X
-Actualitzar l’hora cada segon 
+Actualitzar l’hora cada segon X
 Mostrar la data actual X
-Afegir un botó per ocultar la data actual 
-Afegir un botó per mostrar la data actual
+Afegir un botó per ocultar la data actual x
+Afegir un botó per mostrar la data actualx
 
 */
 
 export default function App() {
   const [update, setUpdate] = useState(0);
+  const [status, setStatus] = useState("");
 
   useEffect(() => {
     setInterval(() => {
@@ -23,17 +24,20 @@ export default function App() {
   const date =
     today.getDate() + ":" + (today.getMonth() + 1) + ":" + today.getFullYear();
 
+  const checkStatus = () =>
+    status === "" ? setStatus("hidden") : setStatus("");
+
   return (
     <>
       <div className="container-digital-watch">
-        <h2 className="digital-watch_time">
+        <h2>
           {today.getHours()} : {today.getMinutes()} : {today.getSeconds()}
         </h2>
-        <p className="digital-watch_date">{date}</p>
-        <button onClick="" className="digital-watch_hideTime">
+        <p className={status}>{date}</p>
+        <button onClick={checkStatus} className={status}>
           Amagar Data
         </button>
-        <button onClick="" className="digital-watch_showTime">
+        <button onClick={checkStatus} className={status === "" ? "hidden" : ""}>
           Veure Data
         </button>
       </div>
